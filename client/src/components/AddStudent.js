79d306e33135ebbe2e8203fbfb4130e392a89205
Sahasra-1011 <sahasra.10.087@gmail.com>
2025-04-27 13:33:08 +0530

    Update AddStudent.js

--- client/src/components/AddStudent.js
+++ client/src/components/AddStudent.js
@@ -23,7 +23,7 @@ const AddStudent = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch('https://student-mangment-abc.onrender.com', {
+    const res = await fetch('https://student-mangment-abc.onrender.com/api/students', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(student),
